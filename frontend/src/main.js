85e28b4d864774eb85467b2bf9fe62af34790dc5
d0rb <hacperu2010@gmail.com>
2021-09-18 15:43:39 -0500

    omg epic starting to create main page ui

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Feed from './feed'
+import './main.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  margin: theme.spacing(1),
+}));
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -12,7 +22,16 @@ class MainPage extends React.Component {
   render() {
     return (
       <Container maxWidth="xl">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        <Box sx={{ height: '100vh' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={7}>
+              <Feed user={this.props.user} />
+            </Grid>
+            <Grid item xs={5}>
+              <Item>rightside</Item>
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
     );
   }
