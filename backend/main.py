a3f4cee55861dad46d6b965d70a8579757b0a1c3
d0rb <hacperu2010@gmail.com>
2021-09-18 02:32:24 -0500

    oOPS !!!!! 🐛
    fixed the bug

--- backend/main.py
+++ backend/main.py
@@ -5,51 +5,55 @@ import os
 import json
 from PIL import Image
 from io import BytesIO
-from functools import cache
+from functools import lru_cache
 from flask import Flask, request
 from sklearn.neighbors import KNeighborsClassifier
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
 
 DATA_FILE = 'data.json'
 
 
 def load_images(file):
     if os.path.exists(file):
-        with open(file, 'r') as rawdata:
-            return json.loads(rawdata.read())
+        with open(file, 'r') as raw_data:
+            return json.loads(raw_data.read())
+
+    global model, preprocess
+    img = preprocess(Image.open('CLIP.png')).unsqueeze(0).to(device)
 
     return {
-        'vector': [],
-        'url': [],
+        'vector': [model.encode_image(img)[0].detach().numpy()],
+        'url': ['https://www.seekpng.com/png/detail/1-16813_dog-png-sit-dog.png'],
     }
 
 
 def fit_knn(image_data, neighbors=10):
-    knn = KNeighborsClassifier(n_neighbors=neighbors)
+    knn_classifier = KNeighborsClassifier(n_neighbors=neighbors)
 
-    knn.fit(image_data['vector'], image_data['url'])
+    knn_classifier.fit(image_data['vector'], image_data['url'])
 
-    return knn
+    return knn_classifier
 
 
-@cache
+@lru_cache(maxsize=None)
 def predict_b64(b64_data):
     img = preprocess(Image.open(BytesIO(base64.b64decode(b64_data)))).unsqueeze(0).to(device)
 
     with torch.no_grad():
-        img_features = model.encode_image(img)
+        img_features = model.encode_image(img)[0]
 
     global knn
 
     return knn.predict([img_features])
 
 
-model, preprocess = clip.load("ViT-B/32", device=device)
 img_dataset = load_images(DATA_FILE)
 knn = fit_knn(img_dataset)
 
 app = Flask(__name__)
+
 
 
 @app.route('/upload_image', methods=['POST'])
@@ -93,3 +97,7 @@ def nearest_text():
         return 'no text!! >:('
 
     return predict_b64(data['image'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
